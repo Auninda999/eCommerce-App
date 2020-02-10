7375128f502079f27858b166ffcc72741db51f94
Auninda999 <auninda123@gmail.com>
2020-02-10 20:25:08 +0600

    First Commit

---
+++ app/src/main/java/com/android/ecommerce/Prevalent/Prevalent.java
@@ -0,0 +1,11 @@
+package com.android.ecommerce.Prevalent;
+
+import com.android.ecommerce.Model.Users;
+
+public class Prevalent {
+
+    public static Users currentOnlineUser;
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+}
